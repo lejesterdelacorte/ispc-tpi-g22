077234e4a8f9dec3589323499d53e0d7d327d8fc
Jonathan Molina <jonathanarmol@gmail.com>
2024-06-12 17:30:41 -0300

    Subo el CRUD branch books. Create no funciona

--- modules/books/createBook.py
+++ modules/books/createBook.py
@@ -1,2 +1,16 @@
+from ..utils.dBConnection import DatabaseConnection
+
 def createBook():
-    print("Crear libro")
+    titulo = input("Ingrese el título del libro: ")
+    autor = input("Ingrese el autor del libro: ")
+    editorial = input("Ingrese la editorial del libro: ")
+    fecha_publicacion =	input("Ingrese la fecha en la que fue publicado el libro: ")
+    genero = input("Ingrese el género al que pertenece el libro: ")
+    usuario_id_usuario = input("Ingrese el id de usuario al que pertenece el libro: ")	
+    db_instance = DatabaseConnection()
+    connection = db_instance.get_connection()
+    cursor = connection.cursor()
+    sql = "INSERT INTO libro (titulo, autor, editorial, fecha_publicacion, genero) VALUES (%s, %s, %s, %s, %s, %s);"
+    values = (titulo, autor, editorial, fecha_publicacion, genero, usuario_id_usuario)
+    cursor.execute(sql, values)
+    connection.commit()
--- modules/books/updateBook.py
+++ modules/books/updateBook.py
@@ -1,2 +1,17 @@
+from ..utils.dBConnection import DatabaseConnection
+
 def updateBook():
-    print("Modificar libro")
+    ID_libro = input("Ingrese el ID del libro a modificar: ")
+    titulo = input("Ingrese el título del libro: ")
+    autor = input("Ingrese el autor del libro: ")
+    editorial = input("Ingrese la editorial del libro: ")
+    fecha_publicacion =	input("Ingrese la fecha en la que fue publicado el libro: ")
+    genero = input("Ingrese el género al que pertenece el libro: ")
+    usuario_id_usuario = input("Ingrese el id de usuario al que pertenece el libro: ")	
+    db_instance = DatabaseConnection()
+    connection = db_instance.get_connection()
+    cursor = connection.cursor()
+    sql = "UPDATE libro SET titulo=%s, autor=%s, editorial=%s, fecha_publicacion=%s, genero=%s, usuario_id_usuario=%s WHERE ID_libro = %s;"
+    values = (titulo, autor, editorial, fecha_publicacion, genero, usuario_id_usuario, ID_libro)
+    cursor.execute(sql, values)
+    connection.commit()
