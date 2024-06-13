e60948c0d9607ebdba179370924784e16a8a0158
Jonathan Molina <jonathanarmol@gmail.com>
2024-06-13 15:34:52 -0300

    Creo carpeta bd

--- modules/books/getBooks.py
+++ modules/books/getBooks.py
@@ -1,9 +1,12 @@
 from ..utils.dBConnection import DatabaseConnection
 
 def getBooks():
+    print("--------CONSULTA DE LIBROS-------") 
     db_instance = DatabaseConnection()
     connection = db_instance.get_connection()
     cursor = connection.cursor() 
     cursor.execute("SELECT * FROM libro;")
     result = cursor.fetchall()
-    print(result) 
+    for i in result:
+        print(i) 
+    print("--------GRACIAS POR CONSULTAR-------")  
